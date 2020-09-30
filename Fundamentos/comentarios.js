1d64a9e8d397c7db1bbc87db73fd7247f16c358e
Willian Alves Couto <wcouto@img.com.br>
2020-09-29 22:36:50 -0300

    Terceiro dia

---
+++ Fundamentos/comentarios.js
@@ -0,0 +1,14 @@
+//Comentario de uma linha
+console.log('Linha 1')
+
+/*
+Comentarios de
+multiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+* Comentario de
+* multiplas linhas
+*/
+console.log('Linha 3')
